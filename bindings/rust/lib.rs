ff0446514cf90d53dc2934c71aceb164d9dd4fb2
Victor Quiroz <git@victorhqc.com>
2025-10-02 21:28:26 +0200

    chore: update docs

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -7,7 +7,7 @@
 //! let code = r#"
 //! "#;
 //! let mut parser = tree_sitter::Parser::new();
-//! let language = tree_sitter_prisma::LANGUAGE;
+//! let language = tree_sitter_prisma_io::LANGUAGE;
 //! parser
 //!     .set_language(&language.into())
 //!     .expect("Error loading Prisma parser");
